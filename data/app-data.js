dd7daf1b77281325fd143db1037f11b09826f469
williamsalebert-prog <williamsalebert@gmail.com>
2026-09-14 23:35:21 -0600

    Add files via upload

---
+++ data/app-data.js
@@ -0,0 +1,29 @@
+window.SETHORIA_DEMO = {
+  categories: [
+    ["personajes","Personajes"],
+    ["grupos","Grupos"],
+    ["arcos","Arcos"],
+    ["eventos","Eventos"],
+    ["timeline-a","Línea de tiempo A"],
+    ["timeline-b","Línea de tiempo B"],
+    ["lugares","Lugares"],
+    ["rutas","Rutas"],
+    ["batallas","Batallas"],
+    ["barcos","Barcos"],
+    ["criaturas-magicas","Criaturas mágicas"],
+    ["criaturas-reales","Criaturas reales"],
+    ["culturas","Culturas y sociedades"],
+    ["armas","Armas"],
+    ["ropa","Ropa"],
+    ["objetos","Objetos"],
+    ["relaciones","Relaciones"],
+    ["historia-real","Historia real"],
+    ["reglas","Reglas de Sethoria"],
+    ["informacion-general","Información general"],
+    ["musica","Música"],
+    ["fuentes","Fuentes de investigación"],
+    ["otros","Otros datos"],
+    ["notas","Notas"]
+  ],
+  seedEntities: []
+};
